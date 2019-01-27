5ba929b980bcd58eb64141e87e58a1adf7372855
Miles Perkins <blackandmiles93@users.noreply.github.com>
2019-01-27 01:34:53 -0500

    trying to figure out how to truncate the description to remove the <br> tags that are coming in with the description

--- components/Ani/AniInfo.js
+++ components/Ani/AniInfo.js
@@ -2,22 +2,49 @@ import React, { Component } from "react";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { StyleSheet, View, Text, Image } from "react-native";
 
-export default class AniInfo extends Component {
+const styles = StyleSheet.create({
+  aniDetailContainer: {
+    display: "flex"
+  }
+});
+
+class AniInfo extends Component {
   constructor(props) {
     super(props);
+  }
+
+  truncateDesc() {
+    let desc = this.props.navigation.getParam("description");
+    // split the description into an array, either by space or each letter (although each would be very not performant)
+    desc = desc.split(" ");
+    // iterate through the array and look for the string "<br> <br>"
+    console.log(desc);
+    // if found
+    //(possibly split into a sub array and then search through that and remove each character within the <br> tag)
+    // else
+    //do nothing
   }
 
   render() {
     const title = this.props.navigation.getParam("title");
     const image = this.props.navigation.getParam("image");
     const description = this.props.navigation.getParam("description");
-    console.log(this.props.navigation.state.params);
+    this.truncateDesc();
+    // console.log(this.props.navigation.state.params);
     return (
-      <View>
-        <Image source={{ uri: image }} style={{ width: 170, height: 170 }} />
-        <Text>{title}</Text>
-        <Text>{description}</Text>
+      <View style={styles.aniDetailContainer}>
+        <View>
+          <Image source={{ uri: image }} style={{ width: 170, height: 200 }} />
+        </View>
+        <View>
+          <Text>{title}</Text>
+        </View>
+        <View>
+          <Text>{description}</Text>
+        </View>
       </View>
     );
   }
 }
+
+export default AniInfo;
