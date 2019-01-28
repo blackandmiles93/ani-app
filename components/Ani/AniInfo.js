f82f963a25a4493de9d09a5158c16263e5651f5b
Miles Perkins <blackandmiles93@users.noreply.github.com>
2019-01-28 00:58:04 -0500

    removed <br> tags but might need refactoring

--- components/Ani/AniInfo.js
+++ components/Ani/AniInfo.js
@@ -5,31 +5,25 @@ import { StyleSheet, View, Text, Image } from "react-native";
 const styles = StyleSheet.create({
   aniDetailContainer: {
     display: "flex"
+  },
+  aniDetailItem: {
+    display: "flex",
+    padding: 10
   }
 });
 
 class AniInfo extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  truncateDesc() {
-    let desc = this.props.navigation.getParam("description");
-    // split the description into an array, either by space or each letter (although each would be very not performant)
-    desc = desc.split(" ");
-    // iterate through the array and look for the string "<br> <br>"
-    console.log(desc);
-    // if found
-    //(possibly split into a sub array and then search through that and remove each character within the <br> tag)
-    // else
-    //do nothing
-  }
+  truncateDesc = desc => {
+    return desc
+      .split("<br>")
+      .sort()
+      .pop();
+  };
 
   render() {
     const title = this.props.navigation.getParam("title");
     const image = this.props.navigation.getParam("image");
     const description = this.props.navigation.getParam("description");
-    this.truncateDesc();
     // console.log(this.props.navigation.state.params);
     return (
       <View style={styles.aniDetailContainer}>
@@ -39,8 +33,8 @@ class AniInfo extends Component {
         <View>
           <Text>{title}</Text>
         </View>
-        <View>
-          <Text>{description}</Text>
+        <View style={styles.aniDetailItem}>
+          <Text>{this.truncateDesc(description)}</Text>
         </View>
       </View>
     );
